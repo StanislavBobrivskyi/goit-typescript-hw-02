81cdf2d0ac795356fbb8a4933352c3c783839289
StanislavBobrivskyi <stanislavbobrivskyi@gmail.com>
2023-10-06 20:16:57 +0200

    1 tasck

--- src/baseTypes/1.ts
+++ src/baseTypes/1.ts
@@ -1,10 +1,20 @@
 // Перетворіть цей код на TypeScript, вказавши відповідні типи для всіх змінних.
 
-let age = 50;
-let name = 'Max';
-let toggle = true;
-let empty = null;
-let notInitialize;
-let callback = (a) => { return 100 + a };
+// let age = 50;
+// let name = 'Max';
+// let toggle = true;
+// let empty = null;
+// let notInitialize;
+// let callback = (a) => { return 100 + a };
 
+// export {};
+let age: number = 50; // age - це число
+let name: string = "Max"; // name - це рядок
+let toggle: boolean = true; // toggle - це булеве значення
+let empty: null = null; // empty - це null
+let notInitialize: undefined; // notInitialize - це undefined
+let callback: (a: number) => number = (a) => {
+  return 100 + a;
+};
 export {};
+// callback - це функція, яка приймає число та повертає число
